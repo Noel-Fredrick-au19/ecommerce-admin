14348ee92432fce53480024a595ab8a1be3d872f
noel.fredrick <noel.fredrick@cheersin.com>
2023-11-12 23:22:13 +0530

    feat: categories-pagetsx changes

--- app/(dashboard)/[storeId]/(routes)/categories/[CategoryId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/[CategoryId]/page.tsx
@@ -1,45 +1,31 @@
 import prismadb from "@/lib/prismadb";
+
 import { CategoryForm } from "./components/category-form";
 
-interface CategoryPageProps {
-  category: any
-  billboards: any
-}
+const CategoryPage = async ({
+  params
+}: {
+  params: { categoryId: string, storeId: string }
+}) => {
+  const category = await prismadb.category.findUnique({
+    where: {
+      id: params.categoryId
+    }
+  });
 
-const CategoryPage: React.FC<CategoryPageProps> = ({ category, billboards }) => {
-  return (
+  const billboards = await prismadb.billboard.findMany({
+    where: {
+      storeId: params.storeId
+    }
+  });
+
+  return ( 
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
         <CategoryForm billboards={billboards} initialData={category} />
       </div>
     </div>
   );
-};
-
-export async function getServerSideProps(context: {
-  params: { categoryId: string; storeId: string };
-}) {
-  const { categoryId, storeId } = context.params;
-
-  const category = await prismadb.category.findUnique({
-    where: {
-      id: categoryId,
-    },
-  });
-
-  const billboards = await prismadb.billboard.findMany({
-    where: {
-      storeId: storeId,
-    },
-  });
-  console.log("🚀 ~ file: page.tsx:30 ~ billboards:", billboards);
-
-  return {
-    props: {
-      category,
-      billboards,
-    },
-  };
 }
 
 export default CategoryPage;
