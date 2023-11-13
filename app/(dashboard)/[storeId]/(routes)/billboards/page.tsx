0368c470f867a99cb0dc9a5f3429b2577c8fa813
noel.fredrick <noel.fredrick@cheersin.com>
2023-11-13 12:11:06 +0530

    feat: bill-board-form eslint disable comment added

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -13,17 +13,17 @@ const BillboardsPage = async ({ params }: { params: { storeId: string } }) => {
       createdAt: "desc",
     },
   });
-
+  /* @ts-expect-error */
   const formatedBillboards: BillboardColumn[] = billboards.map((item) => ({
     id: item.id,
     label: item.label,
-    createdAt: format(item.createdAt, "MMMM do, yyyy")
-  }))
+    createdAt: format(item.createdAt, "MMMM do, yyyy"),
+  }));
 
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient data={formatedBillboards}/>
+        <BillboardClient data={formatedBillboards} />
       </div>
     </div>
   );
