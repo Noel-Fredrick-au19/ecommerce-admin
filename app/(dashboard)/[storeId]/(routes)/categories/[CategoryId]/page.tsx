9892bbbb74ad3de3eb6fb366d3a3077a3005ddda
noel.fredrick <noel.fredrick@cheersin.com>
2023-11-18 13:48:12 +0530

    feat: category issue resolved

--- app/(dashboard)/[storeId]/(routes)/categories/[CategoryId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/[CategoryId]/page.tsx
@@ -3,29 +3,30 @@ import prismadb from "@/lib/prismadb";
 import { CategoryForm } from "./components/category-form";
 
 const CategoryPage = async ({
-  params
+  params,
 }: {
-  params: { categoryId: string, storeId: string }
+  params: { categoryId: string; storeId: string };
 }) => {
+  // /* @ts-expect-error */
   const category = await prismadb.category.findUnique({
     where: {
-      id: params.categoryId
-    }
+      id: params.categoryId !== undefined ? params.categoryId : "",
+    },
   });
 
   const billboards = await prismadb.billboard.findMany({
     where: {
-      storeId: params.storeId
-    }
+      storeId: params.storeId,
+    },
   });
 
-  return ( 
+  return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
         <CategoryForm billboards={billboards} initialData={category} />
       </div>
     </div>
   );
-}
+};
 
 export default CategoryPage;
